1d3d1bd6f110a34e430020dab040a923c0c79bf8
Veronica Andrade <veronica726@gmail.com>
2020-03-04 11:57:45 -0700

    Make UrlContainer into class component and implement the redux store to have access to keys

--- src/components/UrlContainer/UrlContainer.js
+++ src/components/UrlContainer/UrlContainer.js
@@ -1,22 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { setUrls } from '../../actions';
+import { getUrls } from '../../apiCalls';
 import './UrlContainer.css';
 
-const UrlContainer = props => {
-  const urlEls = props.urls.map(url => {
+export class UrlContainer extends Component {
+  constructor(props) {
+    super(props)
+  }
+  componentDidMount() {
+    getUrls()
+      .then(data => this.props.setUrls(data.urls))
+      .catch(err => console.error('Error fetching:', err));
+  }
+  
+  render() {
+    const urlEls = this.props.urls.map(url => {
+      return url.map(u => {
+        return (
+          <div key={u.id} className="url">
+            <h3>{u.title}</h3>
+            <a href={u.short_url} target="blank">{u.short_url}</a>
+            <p>{u.long_url}</p>
+          </div>
+        )
+      })
+    })
     return (
-      <div className="url">
-        <h3>{url.title}</h3>
-        <a href={url.short_url} target="blank">{url.short_url}</a>
-        <p>{url.long_url}</p>
-      </div>
-    )
-  });
-
-  return (
-    <section>
-      { urlEls.length ? urlEls : <p>No urls yet! Find some to shorten!</p> }
-    </section>
-  )
+      <section>
+        { urlEls.length ? urlEls : <p>No urls yet! Find some to shorten!</p> }
+      </section>
+  )}
 }
 
-export default UrlContainer;
+export const mapStateToProps = state =>( {
+  urls: state.urls
+})
+
+export const mapDispatchToProps = dispatch => {
+  return {
+    setUrls: urls => dispatch(setUrls(urls))
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UrlContainer);
