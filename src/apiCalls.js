89566ab5e00f4e5bc6f0deee70fbc7b1f0abee95
Veronica Andrade <veronica726@gmail.com>
2020-03-04 11:55:22 -0700

    Create a post api call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,21 @@
 export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
-      .then(response => response.json())
+    .then(response => response.json())
+}
+
+export const postUrl = (url) => {
+  const long_url = url.urlToShorten
+  const title= url.title
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      long_url: long_url,
+      title: title
+    })
+  }
+  return fetch('http://localhost:3001/api/v1/urls', options)
+    .then(response => response.json())
 }
